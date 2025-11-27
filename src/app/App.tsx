d4ac001b1ef995aeccd351b5f78e18fd1395f140
LCHyeon <lyc3004@naver.com>
2025-11-27 18:21:35 +0900

    feat: app-start-page

---
+++ src/app/App.tsx
@@ -0,0 +1,4 @@
+const App = () => {
+  return <div>start</div>;
+};
+export default App;
--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,19 @@
 import {
   createRouter,
   createRootRoute,
+  createRoute,
 } from "@tanstack/react-router";
+import App from "./app/App";
 
 const rootRoute = createRootRoute();
 
-const routeTree = rootRoute.addChildren([]);
+const appRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/",
+  component: App,
+});
+
+const routeTree = rootRoute.addChildren([appRoute]);
 
 export const router = createRouter({ routeTree });
 export type AppRouter = typeof router;
