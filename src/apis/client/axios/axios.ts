d10b528867a0bd35a1e34952d56c75a8f082bff3
LCHyeon <lyc3004@naver.com>
2025-11-28 23:15:10 +0900

    feat: setup-api-client

---
+++ src/apis/client/axios/axios.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.PUBLIC_BASE_URL ?? "http://localhost:3000",
+  timeout: 10_000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => Promise.reject(error)
+);
+
+export default axiosInstance;
