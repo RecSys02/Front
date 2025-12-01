8da9bf04520cfa32d785c4308fbe080edf587d50
LCHyeon <lyc3004@naver.com>
2025-12-01 19:23:22 +0900

    feat: min-screen-h

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,9 +5,16 @@ import { Outlet } from "@tanstack/react-router";
 const App = () => {
   return (
     <>
-      <Header />
-      <Outlet />
-      <Footer />
+      <div className="min-h-screen flex flex-col w-full max-w-[1920px] mx-auto">
+        <Header />
+        <main className="flex-1">
+          <Outlet />
+        </main>
+      </div>
+
+      <div className="w-full max-w-[1920px] mx-auto">
+        <Footer />
+      </div>
     </>
   );
 };
