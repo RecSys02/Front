168efbeaa170ab3075e9f74673ec78a2641346d0
LCHyeon <lyc3004@naver.com>
2025-12-03 18:44:28 +0900

    feat: content-card

---
+++ src/app/(home)/_components/plan-section/item/content-card.tsx
@@ -0,0 +1,63 @@
+import Column from "@/components/common/container/column";
+import { ContentConfig } from "./content.type";
+import { ImageBox } from "@/components/common/container/image-box";
+import Row from "@/components/common/container/row";
+import Body from "@/components/text/body";
+import { Border } from "@/components/ui/border";
+import Heading from "@/components/text/heading";
+import { HeartIcon } from "lucide-react";
+import { Pill } from "@/components/ui/pill";
+
+type Props = {
+  content: ContentConfig;
+};
+
+const ContentCard = ({ content }: Props) => {
+  return (
+    <Column className="w-140">
+      <ImageBox
+        src={content.imgSrc}
+        className="w-140 h-80 overflow-hidden"
+        fit="cover"
+      />
+      <Row className="w-full h-15 items-center pt-6 px-2.5">
+        <Row className="w-37.5 shrink-0 h-full gap-3 items-center justify-between pr-3">
+          <Body variant="body3" className="w-fit font-medium">
+            POPULAR
+          </Body>
+          <Border className="w-full" />
+        </Row>
+        <Heading
+          variant="heading2"
+          className="w-full flex items-center font-bold fc-gray-800"
+        >
+          {content.title}
+        </Heading>
+        <Row className="w-16 shrink-0 gap-3 justify-start items-center ">
+          <HeartIcon
+            className={
+              content.isActive
+                ? "w-6 h-6 fill-red-500 text-red-500 hover:scale-110"
+                : "w-6 h-6 fill-transparent text-red-500 hover:scale-110"
+            }
+          />
+          <Body variant="body2" className="w-fit fc-gray-800 font-semibold">
+            {content.likes > 99 ? "99+" : content.likes}
+          </Body>
+        </Row>
+      </Row>
+      <Row className="pl-40 py-2.5 gap-x-3 gap-y-1.5 w-full flex-wrap">
+        {content.tags.map((tag, i) => (
+          <Pill
+            key={i}
+            className="w-fit py-0 bg-white border border-gray-300 rounded-full"
+          >
+            <span className="text-body3 font-light text-gray-500">#{tag}</span>
+          </Pill>
+        ))}
+      </Row>
+    </Column>
+  );
+};
+
+export default ContentCard;
