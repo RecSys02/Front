215f8e1b57f29f41c64c0fa01b00164db1c4f8a3
LCHyeon <lyc3004@naver.com>
2025-12-03 22:56:52 +0900

    feat: recommend-button

--- src/app/(home)/_components/discover-section/item/onboarding-carousel-section.tsx
+++ src/app/(home)/_components/discover-section/item/onboarding-carousel-section.tsx
@@ -4,6 +4,10 @@ import Row from "@/components/common/container/row";
 import { DescriptionCarousel } from "./description-carousel";
 import { ImageCarousel } from "./image-carousel";
 import { ONBOARDING_STEPS } from "./onboarding-step";
+import { Button } from "@/components/common/button/button";
+import Column from "@/components/common/container/column";
+import { Link } from "@tanstack/react-router";
+import { ROUTES } from "@/constants/routes";
 
 const OnboardingCarouselSection = () => {
   const [currentIdx, setCurrentIdx] = useState(0);
@@ -14,23 +18,30 @@ const OnboardingCarouselSection = () => {
   const handleDotClick = (idx: number) => api?.scrollTo(idx);
 
   return (
-    <Row className="flex gap-10 px-85 py-20 pt-30">
-      <DescriptionCarousel
-        step={ONBOARDING_STEPS[currentIdx]}
-        currentIdx={currentIdx}
-        total={ONBOARDING_STEPS.length}
-        onPrev={handlePrev}
-        onNext={handleNext}
-      />
+    <Column className="items-center">
+      <Row className="flex gap-10 px-85 py-20 pt-20">
+        <DescriptionCarousel
+          step={ONBOARDING_STEPS[currentIdx]}
+          currentIdx={currentIdx}
+          total={ONBOARDING_STEPS.length}
+          onPrev={handlePrev}
+          onNext={handleNext}
+        />
 
-      <ImageCarousel
-        items={ONBOARDING_STEPS}
-        currentIdx={currentIdx}
-        setIdx={setCurrentIdx}
-        setApi={setApi}
-        onDotClick={handleDotClick}
-      />
-    </Row>
+        <ImageCarousel
+          items={ONBOARDING_STEPS}
+          currentIdx={currentIdx}
+          setIdx={setCurrentIdx}
+          setApi={setApi}
+          onDotClick={handleDotClick}
+        />
+      </Row>
+      <Button className="w-58 h-17 rounded-[36px] bg-primary">
+        <Link to={ROUTES.AI} className="text-[24px] text-white">
+          추천 받기
+        </Link>
+      </Button>
+    </Column>
   );
 };
 
