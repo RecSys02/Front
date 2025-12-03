80795413df17444aa3bcfb23e439f938e8fb8240
LCHyeon <lyc3004@naver.com>
2025-12-03 21:26:26 +0900

    refactor: user-api

--- src/hooks/user.hook.ts
+++ src/hooks/user.hook.ts
@@ -9,17 +9,16 @@ export const useUser = () => {
     queryFn: async () => {
       if (IS_MOCK) {
         return {
-          status: 200,
           body: {
             username: "홍길동",
           },
         };
         // return {
-        //   status: 401,
         //   body: { message: "UNAUTHENTICATED" },
         // };
       }
-      return apiClient.user.me.query();
+      const res = apiClient.user.me.query();
+      return res.body;
     },
   });
 };
