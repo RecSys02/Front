2b21cc5506a0402afe41f1f7cadbbf00a5a68672
LCHyeon <lyc3004@naver.com>
2025-12-03 22:39:55 +0900

    feat: spinner-and-border

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,14 @@
 import Column from "@/components/common/container/column";
 import BannerSection from "./_components/banner-section/banner-section";
 import DiscoverSection from "./_components/discover-section/discover-section";
+import PlanSection from "./_components/plan-section/plan-section";
 
 const Home = () => {
   return (
     <Column className="gap-10">
       <BannerSection />
       <DiscoverSection />
+      <PlanSection />
     </Column>
   );
 };
