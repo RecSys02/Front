688bb0ef0745f98c0d68f17f9e2ee95eee0bf9b8
하나낭 <jone99ovo@gmail.com>
2025-12-05 13:48:19 +0900

    refactor: profile-act

--- src/app/my/_components/profile-section/profile-act-item.tsx
+++ src/app/my/_components/profile-section/profile-act-item.tsx
@@ -1,25 +1,35 @@
-import Column from "@/components/common/container/column";
-import Subtitle from "@/components/text/subtitle";
-import { Link } from "@tanstack/react-router";
 import { ReactNode } from "react";
+import { MYROUTES } from "./my-routes";
+import Map1 from "@/assets/map1.svg?react";
+import Tag from "@/assets/tag.svg?react";
+import Question from "@/assets/question.svg?react";
+//import { IconName } from "lucide-react";
 
-type Props = {
+type ProfileActConfig = {
   act: string;
   icon: ReactNode;
   routeLink: string;
 };
 
-const ProfileActItem = ({act,icon,routeLink}: Props) => {
-    return ( 
-        <Column className="w-55 h-47 items-center justify-center gap-3">
-            <div className="w-21 h-21 bg-gray-100 rounded-[32px] rounded-tr-[0px] flex items-center justify-center">
-                <Link to={routeLink}>
-                    {icon}
-                </Link>
-            </div>
-            <Subtitle variant="subtitle2" className="font-normal fc-gray-700 ">{act}</Subtitle>
-        </Column>
-    );
-};
-
-export default ProfileActItem;
+export const PROFILE_ACT_ITEMS: ProfileActConfig[] = [
+  {
+    act: "저장된 플랜",
+    icon: <Map1/>,
+    routeLink: MYROUTES.Plan
+  },
+  {
+    act: "태그 수정",
+    icon: <Tag/>,
+    routeLink: MYROUTES.Tag
+  },
+  {
+    act: "Q&A",
+    icon: <Question/>,
+    routeLink: MYROUTES.QnA
+  },
+  {
+    act: "Q&A",
+    icon: <Question/>,
+    routeLink: MYROUTES.QnA
+  }
+];
