6cc506a3e4ea6bfaa880bc16a23759bf75c01323
LCHyeon <lyc3004@naver.com>
2025-12-09 16:19:52 +0900

    feat: fix-scroll-top

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,15 @@
 import Footer from "@/components/layout/footer/footer";
 import Header from "@/components/layout/header/header";
-import { Outlet } from "@tanstack/react-router";
+import { Outlet, useLocation } from "@tanstack/react-router";
+import { useEffect } from "react";
 
 const App = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "instant" });
+  }, [pathname]);
+
   return (
     <>
       <div className="w-full min-h-screen overflow-x-auto">
