ab222cac43c18c8a9a7bc28096bd5c49a3358d2d
LCHyeon <lyc3004@naver.com>
2025-12-10 18:57:03 +0900

    refactor: remove-message-from-signin-hook

--- src/app/(auth)/login/_components/login-form.tsx
+++ src/app/(auth)/login/_components/login-form.tsx
@@ -3,6 +3,9 @@ import { useSignin } from "@/hooks/auth.hook";
 import { CustomForm } from "@/components/ui/form/custom-form";
 import { generateLoginFormItems, LoginFormValues } from "./login-item.form";
 import { Surface } from "@/components/ui/surface";
+import { ROUTES } from "@/constants/routes";
+import { useNavigate } from "@tanstack/react-router";
+import { toast } from "sonner";
 
 const LoginForm = () => {
   const [values, setValues] = useState<LoginFormValues>({
@@ -13,16 +16,23 @@ const LoginForm = () => {
 
   const isValid = values.userId.trim() !== "" && values.password.trim() !== "";
   const signin = useSignin();
+  const navigate = useNavigate();
 
   const handleSubmit = async () => {
     if (!isValid) return;
-    console.log("on func", values);
 
-    signin.mutate({
-      userid: values.userId,
-      password: values.password,
-      remember: values.remember,
-    });
+    try {
+      const res = await signin.mutateAsync({
+        userid: values.userId,
+        password: values.password,
+        remember: values.remember,
+      });
+
+      toast.success(`${res.userid}님, 환영합니다!`);
+      navigate({ to: ROUTES.Home });
+    } catch {
+      toast.error("아이디와 비밀번호를 정확히 입력해 주세요.");
+    }
   };
   const items = generateLoginFormItems(values, setValues);
 
