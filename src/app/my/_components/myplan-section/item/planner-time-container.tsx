085814336731737755df1f3eac3ff352366051ad
하나낭 <jone99ovo@gmail.com>
2025-12-16 02:33:36 +0900

    feat: planner-component

---
+++ src/app/my/_components/myplan-section/item/planner-time-container.tsx
@@ -0,0 +1,20 @@
+import Column from "@/components/common/container/column"
+import Row from "@/components/common/container/row";
+import Body from "@/components/text/body";
+
+const TimeContainer = () => {
+    return(
+        <Column className="w-fit">
+            {Array.from({ length: 15 }).map((_, i) => (
+                <Row
+                  key={i}
+                  className="h-11 w-11 border-b border-r justify-center pt-1"
+                >
+                    <Body variant="body3" className="fc-gray-500"> {8+i}:00  </Body>
+                </Row>
+            ))}
+        </Column>
+    )
+}
+
+export default TimeContainer;
