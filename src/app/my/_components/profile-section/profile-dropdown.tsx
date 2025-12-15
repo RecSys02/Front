6198e07d891c92cb3f02cf259abda24bbce17669
하나낭 <jone99ovo@gmail.com>
2025-12-16 02:31:26 +0900

    refactor: add-cursor-pointer

--- src/app/my/_components/profile-section/profile-dropdown.tsx
+++ src/app/my/_components/profile-section/profile-dropdown.tsx
@@ -6,11 +6,11 @@ const ProfileDropdown = () => {
     return(
         <DropdownMenu>
             <DropdownMenuTrigger>
-                <EllipsisVerticalFilled className="h-12 w-12 mt-8 mr-4" strokeWidth={1.0}/>
+                <EllipsisVerticalFilled className="h-12 w-12 cursor-pointer mt-8 mr-4" strokeWidth={1.0}/>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="start" className="bg-white fc-gray-800 p-3">
                 {PROFILE_DROPDOWN_ITEMS.map((item, i) => (
-                    <DropdownMenuItem key={i} className="h-9 hover:bg-gray-100">
+                    <DropdownMenuItem key={i} className="h-9 cursor-pointer hover:bg-gray-100">
                         {item.icon}
                         {item.name}
                     </DropdownMenuItem>
