942794e1eacba53b77248e1eafa5ba53f8e6e6f9
하나낭 <jone99ovo@gmail.com>
2025-12-16 20:48:18 +0900

    refactor: myplan-ui

--- src/app/(my)/_components/myplan-section/item/myplan-item-name.tsx
+++ src/app/(my)/_components/myplan-section/item/myplan-item-name.tsx
@@ -1,14 +1,13 @@
 import MapPin from "@/assets/map-pin.svg?react";
 import Row from "@/components/common/container/row";
-import Subtitle from "@/components/text/subtitle";
-import { ChevronRight } from "lucide-react";
+import Heading from "@/components/text/heading";
 
 const MyPlanName = () => {
     return(
-        <Row className="w-197  h-26 flex items-center gap-4 px-11 pt-2">
+        <Row className="w-fix h-20 flex items-center gap-4 pl-11">
             <MapPin className="w-12 h-12 stroke-width-1" />
-            <Subtitle variant="subtitle2" className="flex items-center">서울역 여행</Subtitle>
-            <ChevronRight size={48} className="rotate-270 ml-auto mr-4"/>
+            <Heading variant="heading2" className=" font-semibold items-center">서울역 여행</Heading>
+            {/*<ChevronRight size={48} className="rotate-270 ml-auto mr-4"/>*/}
         </Row>
     );
 };
