f6379d625aa18e5931e2e816446ab86324c36d90
하나낭 <jone99ovo@gmail.com>
2025-12-16 20:48:18 +0900

    refactor: myplan-ui

--- src/app/(my)/_components/myplan-section/item/myplan-list.tsx
+++ src/app/(my)/_components/myplan-section/item/myplan-list.tsx
@@ -1,13 +1,34 @@
 import Column from "@/components/common/container/column";
 import MyPlanItem from "./myplan-item";
 import MyPlanName from "./myplan-item-name";
+import { Accordion, AccordionContent, AccordionItem } from "@/components/ui/accordion";
+import { MyPlanAccordionTrigger } from "./myplan-title-trigger";
 
 const MyPlanList = () => {
+    const plans = [1,2];
+
     return(
-        <Column className="w-fit items-center border border-primary rounded-2xl pb-4">
-            {/* Accordion */}
-            <MyPlanName />
-            <MyPlanItem /> 
+        <Column className="w-fit items-center border border-primary rounded-2xl">
+            
+            <Accordion
+                type="single"
+                collapsible
+                className="w-full"
+                defaultValue="1"
+            >
+                {plans.map((plan) => (
+                <AccordionItem value={String(plan)}>
+                    <MyPlanAccordionTrigger> 
+                        <MyPlanName /> 
+                    </MyPlanAccordionTrigger>
+                    <AccordionContent className="flex justify-center">
+                        <MyPlanItem /> 
+                    </AccordionContent>
+                </AccordionItem>
+                ))}
+
+            </Accordion>
+        
         </Column>
 
     );
