c622b0b54a8c444c8404fd8c685a7f12c6df9670
LCHyeon <lyc3004@naver.com>
2025-12-25 19:00:15 +0900

    fix: route-structure

--- src/app/(model)/model.layout.tsx
+++ src/app/(model)/model.layout.tsx
@@ -1,7 +1,18 @@
 import { Outlet } from "@tanstack/react-router";
 import { ModelProvider } from "./model.provider";
+import { ModelHistoryStore } from "@/stores/model-history.store";
+import { ModelStore } from "@/stores/model.store";
+import { useEffect } from "react";
+import { ModelInputStore } from "@/stores/model-input.store";
 
 const ModelLayout = () => {
+  useEffect(() => {
+    return () => {
+      ModelStore.actions.clear();
+      ModelHistoryStore.actions.clearHistoryPlaces();
+      ModelInputStore.actions.clearModelInput();
+    };
+  }, []);
   return (
     <ModelProvider>
       <Outlet />
