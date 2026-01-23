9fe83d552c4f74984c45ffbb713355ba22a4cf4c
LCHyeon <lyc3004@naver.com>
2026-01-23 13:15:50 +0900

    fix: bug

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,32 +3,16 @@ import { createRoot } from "react-dom/client";
 import "@/styles/index.css";
 import { router } from "@/router";
 import { RouterProvider } from "@tanstack/react-router";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/sonner";
 import { tsr } from "@/apis/client/ts-rest/client";
 
 const root = createRoot(document.getElementById("root")!);
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 10,
-      gcTime: 1000 * 60 * 30,
-      refetchOnWindowFocus: false,
-      retry: 1,
-    },
-    mutations: {
-      retry: 0,
-    },
-  },
-});
 
 root.render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <tsr.ReactQueryProvider>
-        <RouterProvider router={router} />
-        <Toaster richColors position="top-center" />
-      </tsr.ReactQueryProvider>
-    </QueryClientProvider>
-  </StrictMode>
+    <tsr.ReactQueryProvider>
+      <RouterProvider router={router} />
+      <Toaster richColors position="top-center" />
+    </tsr.ReactQueryProvider>
+  </StrictMode>,
 );
