56ab933dd86917413f122c9d039b11527a8ab786
LCHyeon <lyc3004@naver.com>
2026-01-23 16:17:42 +0900

    fix:model-error

--- src/hooks/model.hook.ts
+++ src/hooks/model.hook.ts
@@ -3,18 +3,16 @@ import { MOCK_MODEL_RESULT } from "@/app/(model)/spot/_components/model.mock";
 import { useMutation } from "@tanstack/react-query";
 import { toast } from "sonner";
 import { ModelResponseDto, ModelRequestDto } from "@/types/model/model.type";
+import { ApiOk } from "@/types/util.type";
 
 const IS_MOCK = import.meta.env.VITE_USE_MOCK === "true";
 
 export const useModel = () => {
   const onError = () => toast.error("추천 생성 중 오류가 발생했습니다.");
 
-  const real = useMutation<ModelResponseDto, Error, { body: ModelRequestDto }>({
-    mutationFn: async ({ body }) => {
-      const res = await tsr.model.generate.mutation({ body });
-      return res.body; 
-    },
+  const real = tsr.model.generate.useMutation({
     onError,
+    select: (res: ApiOk<ModelResponseDto>) => res.body,
   });
 
   const mock = useMutation<ModelResponseDto, Error, { body: ModelRequestDto }>({
