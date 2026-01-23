d714f865a21324eb5ce2f602a83830aa72824e5d
LCHyeon <lyc3004@naver.com>
2026-01-23 16:27:30 +0900

    feat: main-exception-handling

--- src/app/(home)/_components/plan-section/item/content-card-list.tsx
+++ src/app/(home)/_components/plan-section/item/content-card-list.tsx
@@ -5,10 +5,13 @@ import ContentCard from "./content-card";
 import { Spinner } from "@/components/ui/spinner";
 import { Border } from "@/components/ui/border";
 import { Fragment } from "react/jsx-runtime";
+import Body from "@/components/text/body";
+import Heading from "@/components/text/heading";
 
 const ContentCardList = () => {
   const { data, isLoading } = usePopular();
   const contents = data ?? [];
+
   const columns = [
     [contents[0], contents[3]],
     [contents[1], contents[4]],
@@ -21,6 +24,15 @@ const ContentCardList = () => {
         <Row className="w-full justify-center py-10">
           <Spinner />
         </Row>
+      ) : contents.length === 0 ? (
+        <Column className="w-full py-20 items-center justify-center gap-2 min-h-100">
+          <Heading variant="heading2" className="fc-gray-500">
+            아직 인기 여행 플랜이 없습니다.
+          </Heading>
+          <Body variant="body1" className="fc-gray-400">
+            첫 번째 여행 플랜의 주인공이 되어보세요.
+          </Body>
+        </Column>
       ) : (
         <>
           <Column className="flex-1">
