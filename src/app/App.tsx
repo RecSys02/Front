97b1a4dce61a5f18622e66964ea1a9f4d967b77d
LCHyeon <lyc3004@naver.com>
2026-01-26 15:52:19 +0900

    chore: hide-chatbot-launcher-on-model-route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -11,21 +11,20 @@ const App = () => {
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: "instant" });
   }, [pathname]);
-
-  const isFullscreen = pathname.startsWith(ROUTES.ModelSpot);
+  const isModelRoute = pathname.startsWith(ROUTES.ModelSpot);
 
   return (
     <>
       <div className="flex flex-col w-[1920px] mx-auto">
         <div className="min-h-screen flex flex-col w-full mx-auto">
-          {!isFullscreen && <Header />}
-          <main className={isFullscreen ? "flex-1" : "flex flex-1 flex-col"}>
+          {!isModelRoute && <Header />}
+          <main className={isModelRoute ? "flex-1" : "flex flex-1 flex-col"}>
             <Outlet />
           </main>
         </div>
-        {!isFullscreen && <Footer />}
+        {!isModelRoute && <Footer />}
       </div>
-      <ChatbotLauncher />
+      {!isModelRoute && <ChatbotLauncher />}
     </>
   );
 };
