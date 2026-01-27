e97529441c8e08237341427baf7a35b5ba8730f0
LCHyeon <lyc3004@naver.com>
2026-01-27 14:35:38 +0900

    feat: read-user-api

--- src/types/user/user.type.ts
+++ src/types/user/user.type.ts
@@ -1,5 +1,13 @@
 import { z } from "zod";
 
+export const UserSchema = z.object({
+  email: z.string(),
+  userName: z.string(),
+  image: z.string(),
+  tagIds: z.array(z.string()).nullable(),
+});
+export type UserDto = z.infer<typeof UserSchema>;
+
 export const RenameUserSchema = z.object({
   userName: z.string().min(1),
   password: z.string().min(1),
@@ -11,3 +19,13 @@ export const UserMeSchema = z.object({
   image: z.string().nullish(),
 });
 export type UserMeDto = z.infer<typeof UserMeSchema>;
+
+export const UpdateUserTagSchema = z.object({
+  preferredThemes: z.array(z.string()).nullable(),
+  preferredMoods: z.array(z.string()).nullable(),
+  preferredRestaurantTypes: z.array(z.string()).nullable(),
+  preferredCafeTypes: z.array(z.string()).nullable(),
+  avoid: z.array(z.string()).nullable(),
+  activityLevel: z.string().nullable(),
+});
+export type UpdateUserTagDto = z.infer<typeof UpdateUserTagSchema>;
