c101866fda2ec3a6b63ffcd2b89206b6ced7a60b
LCHyeon <lyc3004@naver.com>
2026-01-30 15:14:40 +0900

    fix:api-fetcher

--- src/apis/client/ts-rest/client.ts
+++ src/apis/client/ts-rest/client.ts
@@ -2,18 +2,26 @@ import { ApiFetcherArgs } from "@ts-rest/core";
 import { contract } from "./contract";
 import axiosInstance from "../axios/axios";
 import { initTsrReactQuery } from "@ts-rest/react-query/v5";
-import { ROUTES } from "@/constants/routes";
 
 export const tsr = initTsrReactQuery(contract, {
   baseUrl: "",
   baseHeaders: {},
   api: async (args: ApiFetcherArgs) => {
+    const pathname = args.path.startsWith("http")
+      ? new URL(args.path).pathname
+      : args.path;
     const method = (args.method || "GET").toUpperCase();
 
     const hasQueryInPath = args.path.includes("?");
     const params = hasQueryInPath ? undefined : (args.rawQuery as any);
-    const isModelCall = args.path.startsWith(ROUTES.Model);
-
+    const isModelCall =
+      pathname.startsWith("/__proxy/api/plans") ||
+      pathname.startsWith("/__proxy/api/recommend");
+    console.log("[TSR API PATH]", {
+      method: args.method,
+      pathname,
+      isProxy: pathname.startsWith("/__proxy"),
+    });
     const result = await axiosInstance({
       url: args.path,
       method: method as any,
